eb9d600981a6ed4913f9021a87862caccfe6436f
rock <1318642891@qq.com>
2024-10-14 10:57:21 +0800

    修改

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -117,10 +117,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '音乐频道',
+  HEO_SOCIAL_CARD_TITLE_2: '分享我的音乐',
+  HEO_SOCIAL_CARD_TITLE_3: '用音乐感染人心',
+  HEO_SOCIAL_CARD_URL: 'https://www.baichuanweb.cn/music.html',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
